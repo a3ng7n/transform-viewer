f32556113b0c4a49235215faa815123b3fcf044f
Aaron <aaron@sirrom.systems>
2024-08-08 16:06:06 -0600

    add icon and some shapes

--- src/components/viewport.tsx
+++ src/components/viewport.tsx
@@ -1,12 +1,22 @@
 'use client'
 
+import { Environment, GizmoHelper, GizmoViewport, Grid, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 function Viewport() {
   return (
     <div className="w-full h-full">
-      <Canvas>
-        {null}
+      <Canvas shadows camera={{ position: [5, 5, 5] }}>
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+        <Grid fadeFrom={0} infiniteGrid fadeDistance={25} fadeStrength={5} />
+        <OrbitControls makeDefault />
+        <Environment preset="apartment" />
+        <GizmoHelper alignment="bottom-right">
+          <GizmoViewport />
+        </GizmoHelper>
       </Canvas>
     </div>
   )
