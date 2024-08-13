3dafc9bd92bf5c446cec879a7ea2b2962dd09881
Aaron <aaron@sirrom.systems>
2024-08-13 14:31:06 -0600

    begin implementing 3d objects

--- src/components/viewport.tsx
+++ src/components/viewport.tsx
@@ -1,11 +1,57 @@
-'use client'
+"use client";
 
-import { Environment, GizmoHelper, GizmoViewport, Grid, OrbitControls } from "@react-three/drei";
+import {
+  Environment,
+  GizmoHelper,
+  GizmoViewport,
+  Grid,
+  OrbitControls,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { TransformChainT } from "~/lib/transforms-store";
+import { useTransformStore } from "~/providers/transforms-store-provider";
+
+function RotateQuaternionView() {
+  return <></>;
+}
+
+function TranslateVector3View() {
+  return <></>;
+}
+
+type TransformChainViewProps = {
+  chainIndex: number;
+} & TransformChainT;
+
+function TransformChainView({
+  chainIndex,
+  ...chainData
+}: TransformChainViewProps) {
+  const transformViews = chainData.transforms.map(
+    (transform, transformIndex) => {
+      switch (transform.type) {
+        case "translation": {
+          return <TranslateVector3View />;
+        }
+        case "rotation": {
+          return <RotateQuaternionView />;
+        }
+      }
+    },
+  );
+
+  return <></>;
+}
 
 function Viewport() {
+  const { chains } = useTransformStore((state) => state);
+
+  const chainViews = chains.map((chain, chainIndex) => (
+    <TransformChainView chainIndex={chainIndex} {...chain} />
+  ));
+
   return (
-    <div className="w-full h-full">
+    <div className="h-full w-full">
       <Canvas shadows camera={{ position: [5, 5, 5] }} resize={{ debounce: 0 }}>
         <mesh>
           <boxGeometry />
@@ -19,7 +65,7 @@ function Viewport() {
         </GizmoHelper>
       </Canvas>
     </div>
-  )
+  );
 }
 
 export { Viewport };
